963cfccabb3beb0cc40f41fbadd7b603526e6e84
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2024-05-15 16:48:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 function transition() {
+    var container = document.getElementById('container');
     var overlay = document.getElementById('transition-overlay');
+    var catalog = document.getElementById('product-catalog');
+
+    container.style.opacity = 0;
+    container.style.pointerEvents = 'none';
+
     overlay.style.opacity = 1;
     overlay.style.pointerEvents = 'all';
+
     document.startViewTransition({
         from: 'container',
         to: 'transition-overlay',
         duration: 500
     });
+
     setTimeout(function() {
+        catalog.style.opacity = 1;
+        catalog.style.pointerEvents = 'all';
+
         document.startViewTransition({
             from: 'transition-overlay',
-            to: 'container',
+            to: 'product-catalog',
             duration: 500
         });
+
         overlay.style.opacity = 0;
         overlay.style.pointerEvents = 'none';
     }, 1000);
